8840a11fa6fccef9fdb996ebb355d68c90955d8c
KARABOU Assiou Tchilabalo <assiou-tchilabalo.karabou@ace3i.com>
2016-05-27 15:45:26 +0000

    Creation du repos

---
+++ src/main/java/com/ace3i/smsbanking/SmsbankingApplication.java
@@ -0,0 +1,12 @@
+package com.ace3i.smsbanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsbankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmsbankingApplication.class, args);
+	}
+}
